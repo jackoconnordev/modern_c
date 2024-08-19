a2b20fd1d9152ab54eee512ec7e70e6b6bd7c8bb
Jack O'Connor <github@jackoconnor.dev>
2024-08-19 22:56:11 +0100

    Print UnionFindSet, pass by pointer, ufset_union function
    The -> operator seems quite intuitive when you think of it as `p->ojb ==
    (*p).obj`.

--- acquaintance/lib/unionfind.c
+++ acquaintance/lib/unionfind.c
@@ -1,6 +1,15 @@
 #include "unionfind.h"
 #include <stdbool.h>
+#include <stdio.h>
 #include <stdlib.h>
+
+void printarray_ptr(size_t size, size_t *array) {
+  printf("%s", "[ ");
+  for (size_t i = 0; i < size; ++i) {
+    printf("%zu ", array[i]);
+  }
+  printf("%s", "]\n");
+}
 
 struct UnionFindSet ufset_init(size_t size) {
   bool malloc_failed = false;
@@ -24,9 +33,49 @@ struct UnionFindSet ufset_init(size_t size) {
   return ufset;
 }
 
-void ufset_dealloc(struct UnionFindSet ufset) {
-  free(ufset.elements);
-  free(ufset.sizes);
-  ufset.elements = NULL;
-  ufset.sizes = NULL;
+void ufset_dealloc(struct UnionFindSet *ufset) {
+  free(ufset->elements);
+  free(ufset->sizes);
+  ufset->elements = NULL;
+  ufset->sizes = NULL;
+}
+
+void ufset_print(struct UnionFindSet *ufset) {
+  printarray_ptr(ufset->size, ufset->elements);
+  printarray_ptr(ufset->size, ufset->sizes);
+}
+
+size_t ufset_find(struct UnionFindSet *ufset, size_t element) {
+  size_t parent = ufset->elements[element];
+  while (parent != element) {
+    element = parent;
+    parent = ufset->elements[element];
+  }
+  return parent;
+}
+
+void ufset_union(struct UnionFindSet *ufset, size_t element1, size_t element2) {
+  size_t parent_element1 = ufset->elements[element1];
+  size_t parent_element2 = ufset->elements[element2];
+
+  while (parent_element1 != element1) {
+    element1 = parent_element1;
+    parent_element1 = ufset->elements[element1];
+  }
+
+  while (parent_element2 != element2) {
+    element2 = parent_element2;
+    parent_element2 = ufset->elements[element2];
+  }
+
+  ufset->elements[parent_element1] = parent_element2;
+}
+
+void ufset_find_replace(struct UnionFindSet *ufset, size_t element,
+                        size_t value) {}
+
+void ufset_weighted_union(struct UnionFindSet *ufset, size_t element_1,
+                          size_t element_2) {
+  // text
+  // text
 }
--- acquaintance/lib/unionfind.h
+++ acquaintance/lib/unionfind.h
@@ -11,17 +11,23 @@ struct UnionFindSet {
 
 struct UnionFindSet ufset_init(size_t num_elements);
 
-void ufset_dealloc(struct UnionFindSet ufset);
+void ufset_dealloc(struct UnionFindSet *ufset);
 
-void ufset_print(struct UnionFindSet ufset);
+void ufset_print(struct UnionFindSet *ufset);
 
-void ufset_find(struct UnionFindSet ufset, size_t element);
+size_t ufset_find(struct UnionFindSet *ufset, size_t element);
 
-void ufset_find_replace(struct UnionFindSet ufset, size_t element,
+void ufset_union(struct UnionFindSet *ufset, size_t element1, size_t element2);
+
+void ufset_find_replace(struct UnionFindSet *ufset, size_t element,
                         size_t value);
 
-void ufset_find_compress(struct UnionFindSet ufset, size_t element);
+void ufset_find_compress(struct UnionFindSet *ufset, size_t element);
 
-void ufset_union(struct UnionFindSet ufset, size_t element_1, size_t element_2);
+void ufset_funky_union(struct UnionFindSet *ufset, size_t element_1,
+                       size_t element_2);
+
+void ufset_weighted_union(struct UnionFindSet *ufset, size_t element_1,
+                          size_t element_2);
 
 #endif // !UNIONFIND_H
